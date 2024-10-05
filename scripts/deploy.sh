921404663a66114a0621c3619844c0c48015369b
SW <7469614+sdworth@users.noreply.github.com>
2024-10-05 13:16:26 -0600

    add script for zipping lambda files

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
+BACKGROUND_COLOR="\e[45m"
+TEXT_COLOR="\e[97m"
+END_COLOR="\e[0m"
+
+say () {
+  printf "\n${BACKGROUND_COLOR}${TEXT_COLOR}$1${END_COLOR}"
+}
+
 if aws dynamodb describe-table --table-name checkins &> /dev/null; then
-  echo "Using existing checkins DynamoDB table..."
+  say "using existing checkins DynamoDB table..."
 else
-  echo "Creating checkins DynamoDB table..."
+  say "creating checkins DynamoDB table..."
 
   aws dynamodb create-table \
     --attribute-definitions AttributeName=id,AttributeType=S \
@@ -13,3 +21,14 @@ else
     &> /dev/null
 fi
 
+say "running npm build"
+npm run build
+
+say "zipping lambda files"
+if [ -f function.zip ]; then
+  rm function.zip
+fi
+
+mv dist/index.js index.js
+zip -r function.zip index.js node_modules > /dev/null
+mv index.js dist/index.js
