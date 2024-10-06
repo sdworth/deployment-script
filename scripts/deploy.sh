0296556b28f73898621b94d9ae947f536f6efc37
SW <7469614+sdworth@users.noreply.github.com>
2024-10-05 23:19:22 -0600

    add scripts for creating lambda functions

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,6 +3,7 @@
 BACKGROUND_COLOR="\e[45m"
 TEXT_COLOR="\e[97m"
 END_COLOR="\e[0m"
+DYNAMODB_TABLE_NAME="checkins"
 
 say () {
  printf "\n${BACKGROUND_COLOR}${TEXT_COLOR}~ $1 ~${END_COLOR}"
@@ -43,20 +44,20 @@ aws iam put-role-policy \
   --policy-document file://scripts/policies/logs.json \
   > /dev/null
 
-if aws dynamodb describe-table --table-name checkins &> /dev/null; then
+if aws dynamodb describe-table --table-name $DYNAMODB_TABLE_NAME &> /dev/null; then
   say "using existing checkins DynamoDB table..."
 else
   say "creating checkins DynamoDB table..."
 
   aws dynamodb create-table \
     --attribute-definitions AttributeName=id,AttributeType=S \
-    --table-name checkins \
+    --table-name $DYNAMODB_TABLE_NAME \
     --key-schema AttributeName=id,KeyType=HASH \
     --billing-mode PAY_PER_REQUEST \
     > /dev/null
 fi
 
-if aws dynamodb describe-table --table-name checkins &> /dev/null; then
+if aws dynamodb describe-table --table-name $DYNAMODB_TABLE_NAME &> /dev/null; then
   say "confirmed dynamodb creation succeeded"
 else
   say "did not successfully create dynamodb table, exiting"
@@ -64,7 +65,7 @@ else
 fi
 
 say "running npm build"
-npm run build
+npm run build > /dev/null
 
 say "zipping lambda files"
 if [ -f function.zip ]; then
@@ -74,3 +75,22 @@ fi
 mv dist/index.js index.js
 zip -r function.zip index.js node_modules > /dev/null
 mv index.js dist/index.js
+
+say "creating lambdas..."
+aws lambda create-function \
+  --function-name checkins-backend \
+  --runtime nodejs20.x \
+  --zip-file fileb://function.zip \
+  --handler index.backend \
+  --role arn:aws:iam::${AWS_ACCOUNT_ID}:role/lambda-read \
+  --environment Variables={DYNAMO_TABLE_NAME=$DYNAMODB_TABLE_NAME} \
+  > /dev/null
+
+aws lambda create-function \
+  --function-name create-checkin \
+  --runtime nodejs20.x \
+  --zip-file fileb://function.zip \
+  --handler index.checkins \
+  --role arn:aws:iam::${AWS_ACCOUNT_ID}:role/lambda-read+write \
+  --environment Variables={DYNAMO_TABLE_NAME=$DYNAMODB_TABLE_NAME} \
+  > /dev/null
