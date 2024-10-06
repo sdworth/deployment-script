a16307779d6b9033b8c7614e8355e8b5d10c653c
SW <7469614+sdworth@users.noreply.github.com>
2024-10-05 22:57:03 -0600

    add iam role automation for lambdas

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,8 +5,43 @@ TEXT_COLOR="\e[97m"
 END_COLOR="\e[0m"
 
 say () {
-  printf "\n${BACKGROUND_COLOR}${TEXT_COLOR}$1${END_COLOR}"
+ printf "\n${BACKGROUND_COLOR}${TEXT_COLOR}~ $1 ~${END_COLOR}"
 }
+
+say "creating iam roles..."
+aws iam create-role \
+  --role-name lambda-read \
+  --assume-role-policy-document file://scripts/policies/lambda-trust-policy.json
+  > /dev/null
+
+aws iam create-role \
+  --role-name lambda-read+write \
+  --assume-role-policy-document file://scripts/policies/lambda-trust-policy.json
+  > /dev/null
+
+aws iam put-role-policy \
+  --role-name lambda-read \
+  --policy-name dynamodb-read \
+  --policy-document file://scripts/policies/dynamodb-read.json \
+  > /dev/null
+
+aws iam put-role-policy \
+  --role-name lambda-read+write \
+  --policy-name dynamodb-read+write \
+  --policy-document file://scripts/policies/dynamodb-read-write.json \
+  > /dev/null
+
+aws iam put-role-policy \
+  --role-name lambda-read \
+  --policy-name logs \
+  --policy-document file://scripts/policies/logs.json \
+  > /dev/null
+
+aws iam put-role-policy \
+  --role-name lambda-read+write \
+  --policy-name logs \
+  --policy-document file://scripts/policies/logs.json \
+  > /dev/null
 
 if aws dynamodb describe-table --table-name checkins &> /dev/null; then
   say "using existing checkins DynamoDB table..."
@@ -18,7 +53,14 @@ else
     --table-name checkins \
     --key-schema AttributeName=id,KeyType=HASH \
     --billing-mode PAY_PER_REQUEST \
-    &> /dev/null
+    > /dev/null
+fi
+
+if aws dynamodb describe-table --table-name checkins &> /dev/null; then
+  say "confirmed dynamodb creation succeeded"
+else
+  say "did not successfully create dynamodb table, exiting"
+  exit 0
 fi
 
 say "running npm build"
